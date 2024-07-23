c17ec3835c920e86c01c7aa61130aeaa4c916ca6
Toseefhusen <toseefhusen@rangam.com>
2024-07-23 15:39:23 +0530

    Initial Commit

---
+++ TKDocumentParserDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TKDocumentParserDemo
+//
+//  Created by Toseef on 23/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TKDocumentParserDemo/TKDocumentParserDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TKDocumentParserDemoApp.swift
+//  TKDocumentParserDemo
+//
+//  Created by Toseef on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TKDocumentParserDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
